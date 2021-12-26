850164a4bdf041d8e32dfbb52f2c3c2b65c0560f
hamad <7amad7arisi@gmail.com>
2021-12-26 15:28:21 +0300

    with... UserStories and Wireframes and ERD Digram

---
+++ NextWatch/NextWatch/Controller/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  loginViewController.swift
+//  NextWatch
+//
+//  Created by حمد الحريصي on 25/12/2021.
+//
+
+import Foundation
+import UIKit
+
+
+class LoginViewController: UIViewController
+{
+    
+}
